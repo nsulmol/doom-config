0f1bee9fe865b670b58d7a1eb2a7cbf314ef4d96
nsulmol <nsulmol@nsulmol-debian>
2023-03-29 15:34:07 -0400

    feat: Minor changes for minimal config.el
    - Change name (to be me)
    - Change theme (to one I like)
    - Add fullscreen on startup
    - Lower time-to-show which-key (to 0.33s)

--- config.el
+++ config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Nick Sullivan"
+      user-mail-address "nsulmol@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-zenburn)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -40,7 +40,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq org-directory "~/Org/")
 
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
@@ -74,3 +74,10 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+;;
+
+;; Set up quick which-key menu
+(setq which-key-idle-delay 0.33)
+
+;; Maximize emacs on startup
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
